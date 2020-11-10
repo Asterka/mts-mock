0968a9ea60e5d91ea1d50760ce2de3d61e67806f
asterka <askar.master@yandex.ru>
2020-11-10 04:08:20 -0500

    modified:   .gitignore 	modified:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   src/Main.js 	new file:   src/index.js

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import react from "react"
+import ReactDOM from 'react-dom';
+
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Helo</h1>
+            </div>
+        )
+    }
+}

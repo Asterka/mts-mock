8f36fd15c929b383eac19f65e5259f3b0d3556a9
asterka <askar.master@yandex.ru>
2020-11-23 08:51:02 -0500

    modified:   src/components/Document.js 	modified:   src/components/Documents.js 	modified:   src/styles/App.scss 	modified:   src/styles/_documents.scss 	modified:   src/styles/_header.scss 	modified:   src/styles/_search_component.scss 	modified:   src/styles/_table_description.scss

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,3 +1,4 @@
+import { lightGreen } from '@material-ui/core/colors'
 import React from 'react'
 
 export default function Document({number, clientName, issueDate, issueStatus}) {
@@ -6,7 +7,7 @@ export default function Document({number, clientName, issueDate, issueStatus}) {
                 <h2 className="number">{number}</h2>
                 <h2 className="client_name">{clientName}</h2>
                 <h2 className="issue_date">{issueDate}</h2>
-                <h2 className="issue_status">{issueStatus}</h2>
+                <h2 className="issue_status" style={{color: issueStatus==="Подписан"?"#00C19B":"#FA8A64"}}>{issueStatus}</h2>
             </div>
     )
 }
--- src/components/Documents.js
+++ src/components/Documents.js
@@ -10,6 +10,15 @@ export default function Documents({number, clientName, issueDate, issueStatus}) 
         {number:"3265409529", clientName:"Веселов Роман Александрович",issueDate:"16.10.2020",issueStatus:"Подписан"},
        
         {number:"3265367895", clientName:"Росляков Михаил Игоревич",issueDate:"16.10.2020",issueStatus:"Подписан"},
+
+        {number:"3265482010", clientName:"Николаев Владимир Петрович",issueDate:"",issueStatus:"Ожидает подписания"},
+
+        {number:"32654812672", clientName:"Лебедева Светлана Ивановна",issueDate:"",issueStatus:"Ожидает подписания"},
+
+        {number:"3265409529", clientName:"Веселов Роман Александрович",issueDate:"16.10.2020",issueStatus:"Подписан"},
+       
+        {number:"3265367895", clientName:"Росляков Михаил Игоревич",issueDate:"16.10.2020",issueStatus:"Подписан"},
+        {number:"3265482010", clientName:"Николаев Владимир Петрович",issueDate:"",issueStatus:"Ожидает подписания"}
     ].map((element)=>{
         return(
             <Document number={element.number} clientName={element.clientName} issueDate={element.issueDate} issueStatus={element.issueStatus}/>

8e92dfbee34d5eac4234ffe88120ebda2e56bf1e
asterka <askar.master@yandex.ru>
2020-11-23 13:50:49 -0500

    modified:   src/components/Document.js 	modified:   src/styles/_documents.scss 	modified:   src/styles/_header.scss 	modified:   src/styles/_search_component.scss

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,4 +1,4 @@
-import { lightGreen } from '@material-ui/core/colors'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import React from 'react'
 
 export default function Document({number, clientName, issueDate, issueStatus}) {
@@ -8,6 +8,9 @@ export default function Document({number, clientName, issueDate, issueStatus}) {
                 <h2 className="client_name">{clientName}</h2>
                 <h2 className="issue_date">{issueDate}</h2>
                 <h2 className="issue_status" style={{color: issueStatus==="Подписан"?"#00C19B":"#FA8A64"}}>{issueStatus}</h2>
+                <div className="show_more">
+                <MoreVertIcon />
+                </div>
             </div>
     )
 }

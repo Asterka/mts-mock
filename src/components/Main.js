4c3010b94b3944ae019e72edd4e2af4b5d0bc5f5
asterka <askar.master@yandex.ru>
2020-11-23 07:39:40 -0500

    modified:   src/components/Document.js 	new file:   src/components/Documents.js 	modified:   src/components/Main.js 	deleted:    src/styles/_document.scss 	new file:   src/styles/_documents.scss 	modified:   src/styles/_main.scss 	modified:   src/styles/_table_description.scss

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchItemSection from "./SearchItemSection";
 import DescriptionSection from "./DescriptionSection";
-
+import Documents from './Documents';
 export default function Main() {
     return (
         <div className="main">
           <SearchItemSection/>
           <DescriptionSection/>
+          <Documents />
           <hr />
         </div>
     )
